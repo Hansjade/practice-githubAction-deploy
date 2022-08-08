a8fabf06730a13510777f3a63442e2861cd2a8dc
Hansjade <Hanssstt@gmail.com>
2022-08-08 16:06:20 +0900

    githubAction autoDeploy third try

--- src/main/java/com/codestates/practicegithubActiondeploy/controller/HelloController.java
+++ src/main/java/com/codestates/practicegithubActiondeploy/controller/HelloController.java
@@ -1,2 +1,17 @@
-package com.codestates.practicegithubActiondeploy.controller;public class HelloController {
+package com.codestates.practicegithubActiondeploy.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Sohyun nim. Bless you!!!";
+        // 소현님 얼른 나으세요ㅠㅠ
+        // 그렇지만 다음주에 저는 같이 페어를 안할것같...
+        // 주워 담겠습니다.
+    }
+
 }
